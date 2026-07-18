d7fab8ed1fb42fdc045a832acc258fc3dae8b5fc
ClaireHappel <clairehap1@gmail.com>
2026-07-18 13:35:11 -0500

    add / babel preset for typescript

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
             presets: [
               '@babel/preset-env',
               '@babel/preset-react',
+              '@babel/preset-typescript',
             ],
           },
         },
