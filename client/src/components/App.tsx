cedfe367b32babe01fc93a55f10b32248e2597f2
ClaireHappel <clairehap1@gmail.com>
2026-07-22 19:16:47 -0500

    update / to imports and color + styling

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,13 +1,25 @@
+/* eslint-disable object-curly-newline */
 import React from 'react';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import ThemeProvider from '@mui/system/ThemeProvider';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import theme from '../theme';
+
+// component imports
 import NavBar from './NavBar/NavBar';
+import Posts from './Posts/Posts';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <NavBar />
+      <Box component="main" sx={{ pt: 16, pb: 8, backgroundColor: '#bfc9e2', minHeight: '100vh' }}>
+        <Container maxWidth="md">
+          <Posts />
+        </Container>
+      </Box>
     </ThemeProvider>
   );
 }
