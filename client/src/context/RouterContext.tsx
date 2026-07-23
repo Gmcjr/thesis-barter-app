6671f6a91056243127f3c5f06cc2d57db28e7125
dzaeltic <devind224@gmail.com>
2026-07-23 17:35:11 -0500

    add / context and provider to router

--- client/src/context/RouterContext.tsx
+++ client/src/context/RouterContext.tsx
@@ -1,5 +1,5 @@
-import {
-  createContext, useContext, useEffect, useState, type ReactNode,
+import React, {
+  createContext, useContext, useState, type ReactNode,
 } from 'react';
 
 interface RouterContextValue {
@@ -7,10 +7,25 @@ interface RouterContextValue {
   navigate: (to: string) => void;
 }
 
-export function RouterProvider() {
-  const [path, setPath] = useState(window.location.pathname)
-};
+const RouterContext = createContext<RouterContextValue | undefined>(undefined);
+
+export function RouterProvider({ children }: { children: ReactNode }) {
+  const [path, setPath] = useState(window.location.pathname);
+
+  const navigate = (to: string) => {
+    if (to === window.location.pathname) return;
+    setPath(to);
+  };
+
+  return (
+    <RouterContext.Provider value={{ path, navigate }}>
+      {children}
+    </RouterContext.Provider>
+  );
+}
 
 export function useRouter() {
-
+  const ctx = useContext(RouterContext);
+  if (!ctx) throw new Error('Outside of router context');
+  return ctx;
 }
