53cc28d5a1b5c050bb90c0cf3674ee272354f3d2
dzaeltic <devind224@gmail.com>
2026-07-24 15:44:42 -0500

    start / styling out profile page

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,10 +1,64 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 function Profile() {
-
   return (
-    <p>HELLO THERE</p>
-  )
+    <Box sx={{ width: '100%', mt: -4 }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          allignItems: 'center',
+          gap: 1,
+        }}
+      >
+        <Avatar
+          sx={{
+            bgcolor: 'primary.main',
+            width: 100,
+            height: 100,
+            fontSize: '2.4rem',
+          }}
+        >
+          D
+        </Avatar>
+        <Card>
+          <CardContent>
+            <Typography>
+              Devin Delgado
+            </Typography>
+            <Typography>
+
+              User since: 1468
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          allignItems: 'center',
+          gap: 1,
+        }}
+      >
+        <Box>
+          <Card>
+            <CardContent>
+              <Typography>
+                POST
+              </Typography>
+            </CardContent>
+          </Card>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 export default Profile;
